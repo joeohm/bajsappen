ee643905bcf35a0d30220c82eb7eab012407c8c0
joeohm <j.ohman@hotmail.com>
2021-05-14 21:05:19 +0200

    Styles the home page with our main bajs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <p>bajs home</p>;
+  return (
+    <>
+      <div className="bajs"></div>
+      <div className="bajscontent">
+        <h1>Welcome to Bajsappen!</h1>
+        <p>
+          All in one solution for sending carefully curated bajsklumps to others
+          and keeping track of your own!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Home;
