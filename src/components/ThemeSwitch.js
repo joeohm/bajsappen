e7e0a9a0d767c5fd960b9816687530617f9ed02c
joeohm <j.ohman@hotmail.com>
2021-05-14 20:44:16 +0200

    Implements the theme switching on button click

--- src/components/ThemeSwitch.js
+++ src/components/ThemeSwitch.js
@@ -1,5 +1,14 @@
+import { setTheme } from "../utils/theme";
+
 const ThemeSwitch = () => {
-  return <button>Switch theme</button>;
+  const handleOnClick = () => {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-light");
+    } else {
+      setTheme("theme-dark");
+    }
+  };
+  return <button onClick={() => handleOnClick()}>Switch theme</button>;
 };
 
 export default ThemeSwitch;
