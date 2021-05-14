ed3339c3efa8fa7ce8f863bb599547accaad663d
joeohm <j.ohman@hotmail.com>
2021-05-14 20:10:09 +0200

    Styling for background and navbar

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import GetStarted from "./pages/GetStarted";
 import Users from "./pages/Users";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./assets/style.css";
 
 export default function App() {
   return (
     <Router>
       <div>
         <nav>
-          <ul>
-            <li>
+          <ul className="navbar">
+            <li className="nav-link">
               <Link to="/">Home</Link>
             </li>
-            <li>
+            <li className="nav-link">
               <Link to="/get-started">Get started</Link>
             </li>
-            <li>
+            <li className="nav-link">
               <Link to="/my-bajsklumps">My bajsklumps</Link>
             </li>
-            <li>
+            <li className="nav-link">
               <Link to="/users">Users</Link>
             </li>
           </ul>
