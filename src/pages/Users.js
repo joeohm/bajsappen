1c6fd968ab4c9749fdc7d274d4437ab4baea5f47
joeohm <j.ohman@hotmail.com>
2021-05-16 12:19:03 +0200

    Implements the databajs

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
 const Users = () => {
-  return <p>bajs Users</p>;
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_DB}/users`)
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  });
+  const [users, setUsers] = useState([]);
+  return users.map((user, i) => {
+    return (
+      <p key={i}>
+        Hej, {user.name}. You are user number {user.id}
+      </p>
+    );
+  });
 };
 
 export default Users;
